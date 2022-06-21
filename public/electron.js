de8c61a68362c25c68d8c6f2329f1fd1a6ca64f9
= <cinarrtolga@gmail.com>
2022-06-21 20:07:08 +0100

    Electron auto updater integration

--- public/electron.js
+++ public/electron.js
@@ -1,6 +1,10 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 const url = require('url');
+const { autoUpdater } = require("electron-updater");
+
+let updateInterval = null;
+
 function createWindow() {
     const startUrl = process.env.ELECTRON_START_URL || url.format({
         pathname: path.join(__dirname, '../index.html'),
@@ -22,7 +26,11 @@ function createWindow() {
     });
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+    createWindow();
+    updateInterval = setInterval(() => autoUpdater.checkForUpdates(), 600000);
+});
+
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit()
@@ -33,4 +41,28 @@ app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
         createWindow()
     }
+});
+
+autoUpdater.on("update-available", (_event, releaseNotes, releaseName) => {
+    const dialogOpts = {
+		type: 'info',
+		buttons: ['Ok'],
+		title: 'Update Available',
+		message: process.platform === 'win32' ? releaseNotes : releaseName,
+		detail: 'A new version download started. The app will be restarted to install the update.'
+	};
+	dialog.showMessageBox(dialogOpts);
+})
+
+autoUpdater.on("update-downloaded", (_event, releaseNotes, releaseName) => {
+	const dialogOpts = {
+		type: 'info',
+		buttons: ['Restart', 'Later'],
+		title: 'Application Update',
+		message: process.platform === 'win32' ? releaseNotes : releaseName,
+		detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+	};
+	dialog.showMessageBox(dialogOpts).then((returnValue) => {
+		if (returnValue.response === 0) autoUpdater.quitAndInstall()
+	})
 });
