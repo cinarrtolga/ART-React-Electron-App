d1dc9d92fae3851a768bd6e4908236142050adbc
= <cinarrtolga@gmail.com>
2022-07-06 11:24:54 +0100

    A new version.

--- public/electron.js
+++ public/electron.js
@@ -23,7 +23,8 @@ function createWindow() {
         width: 800,
         height: 800,
         webPreferences: {
-            nodeIntegration: true
+            nodeIntegration: true,
+            contextIsolation: false
         }
     });
 
